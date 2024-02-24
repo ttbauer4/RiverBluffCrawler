2d968f25233baae9ac99a696d341073696870ff2
Trenton Bauer <trenton.bauer@gmail.com>
2024-02-24 12:18:04 -0600

    added exception handling and descriptive messages

--- crawler.py
+++ crawler.py
@@ -1,3 +1,5 @@
+import sys
+import os
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
@@ -6,22 +8,42 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 
-# create new Firefox driver
-fireFoxOptions = Options()
-fireFoxOptions.add_argument("--headless")
-driver = webdriver.Firefox(options=fireFoxOptions, service=
-    Service(GeckoDriverManager().install()))
+try:
+    # create new Firefox driver
+    fireFoxOptions = Options()
+    fireFoxOptions.add_argument("--headless")
+    driver = webdriver.Firefox(options=fireFoxOptions, service=
+        Service(GeckoDriverManager().install()))
 
-# navigate to home page
-driver.get('https://www.riverblufflandscaping.com/')
+    # navigate to home page
+    driver.get('https://www.riverblufflandscaping.com/')
 
-# click to blog
-to_blog = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Blog")))
-to_blog.click()
+except:
+    print("\nFAILED TO INITIALIZE WEBDRIVER\n")
+    sys.exit()
 
-# click to latest blog post
-blog_link = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "i6wKmL")))
-driver.execute_script("arguments[0].click();", blog_link)
+try:
+    # click to blog
+    to_blog = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.LINK_TEXT, "Blog")))
+    to_blog.click()
+
+except:
+    print("\nFAILED TO CLICK BLOG LINK ON HOME PAGE\n")
+    driver.close()
+    sys.exit()
+
+try:
+    # click to latest blog post
+    blog_link = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "i6wKmL")))
+    driver.execute_script("arguments[0].click();", blog_link)
+
+except:
+    print("\nFAILED TO CLICK LATEST BLOG POST\n")
+    driver.close()
+    sys.exit()
 
 # close driver
 driver.close()
+print("Crawl successful.\n")
+print("Using IP:\n")
+print(os.system('ipconfig'))
